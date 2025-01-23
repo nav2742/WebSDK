b4608f3b1801302c39037bc82aa889a15462a226
yaswanth <ykothakota198@gmail.com>
2025-01-23 20:28:55 +0530

    fisrt commit

---
+++ UI/kore-config.js
@@ -0,0 +1,152 @@
+(function(KoreSDK){
+    let query_str = window.location.search
+    let param = new URLSearchParams(query_str)
+    console.log(param.get("fs"))
+    function getCookie(name) {
+        const nameEQ = name + "=";
+        const ca = document.cookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i].trim();
+            if (c.indexOf(nameEQ) === 0) {
+                return decodeURIComponent(c.substring(nameEQ.length, c.length));
+            }
+        }
+        return null;  // Return null if cookie not found
+    }
+    
+    var storedData = getCookie("naveen");
+    var loginData
+    if (storedData) {
+        loginData = JSON.parse(storedData);  // Deserialize the cookie value back to an object
+        console.log(loginData);
+    }
+    function koreGenerateUUID() {
+        var d = new Date().getTime();
+        if (window.performance && typeof window.performance.now === "function") {
+        d += performance.now(); //use high-precision timer if available
+        }
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+        });
+        return uuid;
+        }
+		
+    var KoreSDK=KoreSDK||{};
+
+    var botOptions = {};
+    botOptions.logLevel = 'debug';
+    botOptions.koreAPIUrl = "https://bots.kore.ai/api/";
+    botOptions.koreSpeechAPIUrl = "";//deprecated
+    //botOptions.bearer = "bearer xyz-------------------";
+    //botOptions.ttsSocketUrl = '';//deprecated
+    botOptions.koreAnonymousFn = koreAnonymousFn;
+    botOptions.recorderWorkerPath = '../libs/recorderWorker.js';
+    botOptions.enableAck={ // set true, to send acknowledgment to server on receiving response from bot 
+        delivery:false
+    }
+    // To add query parameters for the websocket url, add the query parameters in queryParams object
+    botOptions.webSocketConfig = {
+        socketUrl: {
+            queryParams: {}
+        }
+    }
+
+    botOptions.JWTUrl = "http://demo.kore.net:3000/users/sts";
+    botOptions.userIdentity = koreGenerateUUID();// Provide users email id here
+    botOptions.botInfo = { name: "ABC Banking Ltd", "_id": "st-987088dc-bd70-59ea-b05e-82677a3131e9",customData:{
+        token:loginData
+    } }; // bot name is case sensitive
+
+    /* 
+    Important Note: These keys are provided here for quick demos to generate JWT token at client side but not for Production environment.
+    Refer below document for JWT token generation at server side. Client Id and Client secret should maintained at server end.
+    https://developer.kore.ai/docs/bots/sdks/user-authorization-and-assertion/
+    **/
+    botOptions.clientId = "cs-4496f8e8-a8b9-5d26-92f0-d4ed5660aca5";
+    botOptions.clientSecret = "iQM8ThGrLpB7AYw2QHH6c7pAUJnNtQIwSMkPrkZPiFQ=";
+    botOptions.brandingAPIUrl = botOptions.koreAPIUrl +'websdkthemes/'+  botOptions.botInfo._id+'/activetheme';
+    botOptions.enableThemes = true;
+// for webhook based communication use following option 
+// botOptions.webhookConfig={
+//     enable:true,
+//     webhookURL:'PLEASE_PROVIDE_WEBHOOK_URL',
+//     useSDKChannelResponses: false, //Set it to true if you would like to use the responses defined for Web/Mobile SDK Channel
+//     apiVersion:2 //webhookURL will be converted to v2 by default. To use v1(not recommended) webhookURL change it to 1
+// }
+   
+// To modify the web socket url use the following option
+// botOptions.reWriteSocketURL = {
+//     protocol: 'PROTOCOL_TO_BE_REWRITTEN',
+//     hostname: 'HOSTNAME_TO_BE_REWRITTEN',
+//     port: 'PORT_TO_BE_REWRITTEN'
+// };
+    
+    var chatConfig={
+        botOptions:botOptions,
+        allowIframe: false, 			// set true, opens authentication links in popup window, default value is "false"
+        isSendButton: false, 			// set true, to show send button below the compose bar
+        isTTSEnabled: true,			// set true, to show speaker icon
+        ttsInterface:'webapi',          // webapi or awspolly , where default is webapi
+        isSpeechEnabled: true,			// set true, to show mic icon
+        stt:{
+            vendor: 'webapi',           //'webapi'|'azure'|'google' //uses respective settings from the following keys and uncomments respective files in index.html
+            azure:{
+                subscriptionKey: '',
+                recognitionLanguage: 'en-US',
+                recognitionMode: 'Interactive' //Interactive/Dictation/Conversation/Interactive
+            },
+           google:{
+            apiKey:"",
+            recognitionLanguage:"en-US"
+           },
+           webapi:{
+            recognitionLanguage: 'en-US'
+           }
+        },
+        allowLocation: true,			// set false, to deny sending location to server
+        loadHistory: false,				// set true to load recent chat history
+        messageHistoryLimit: 10,		// set limit to load recent chat history
+        autoEnableSpeechAndTTS: false, 	// set true, to use talkType voice keyboard.
+        graphLib: "d3" ,				// set google, to render google charts.This feature requires loader.js file which is available in google charts documentation.
+        googleMapsAPIKey:"",
+        minimizeMode: param.get("fs")?false:true,             // set true, to show chatwindow in minimized mode, If false is set remove #chatContainer style in chatwindow.css  
+        multiPageApp: {
+            enable: false,              //set true for non SPA(Single page applications)
+            userIdentityStore: 'localStorage',//'localStorage || sessionStorage'
+            chatWindowStateStore: 'localStorage'//'localStorage || sessionStorage'
+        },              
+        supportDelayedMessages:true,    // enable to add support for renderDelay in message nodes which will help to render messages with delay from UI       
+        maxTypingIndicatorTime:10000,   //time in milliseconds,typing indicator will be stopped after this time limit,even bot doesn't respond 
+        pickersConfig:{
+            showDatePickerIcon:false,           //set true to show datePicker icon
+            showDateRangePickerIcon:false,      //set true to show dateRangePicker icon
+            showClockPickerIcon:false,          //set true to show clockPicker icon
+            showTaskMenuPickerIcon:false,       //set true to show TaskMenu Template icon
+            showradioOptionMenuPickerIcon:false //set true to show Radio Option Template icon
+        },
+        sendFailedMessage:{
+            MAX_RETRIES:3
+        },
+        maxReconnectionAPIAttempts: 5,  // Number of retries on api failure,
+        syncMessages: {
+            onReconnect: {
+                enable: false,  // Set true to sync messages on Reconnect
+                batchSize: 10   // To configure the number of messages to fetch
+            },
+            onNetworkResume: {
+                enable: true,  // Set true to sync messages on network back
+                batchSize: 10   // To configure the number of messages to fetch
+            }
+        },
+        showAttachment: true  // Set false, to hide attachment icon
+    };
+     /* 
+        allowGoogleSpeech will use Google cloud service api.
+        Google speech key is required for all browsers except chrome.
+        On Windows 10, Microsoft Edge will support speech recognization.
+     */
+
+    KoreSDK.chatConfig=chatConfig
+})(window.KoreSDK);
