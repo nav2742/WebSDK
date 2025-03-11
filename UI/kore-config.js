a0a793d8f61d064da9c59347db4f08b3517cb860
yaswanth <ykothakota198@gmail.com>
2025-03-11 09:25:56 -0400

    cemex

--- UI/kore-config.js
+++ UI/kore-config.js
@@ -54,7 +54,7 @@
     // botOptions.JWTUrl = "http://demo.kore.net:3000/users/sts";
     botOptions.JWTUrl= "https://sdkapp.onrender.com/api/users/sts"
     botOptions.userIdentity = old_id?old_id: koreGenerateUUID();// Provide users email id here
-    botOptions.botInfo = { name: "ABC BANK", "_id": "st-987088dc-bd70-59ea-b05e-82677a3131e9",customData:{
+    botOptions.botInfo = { name: "Cemex", "_id": "st-c533118e-d3bd-53f9-a1c7-e4d9e6be4f3f",customData:{
         token:storedData?JSON.parse(storedData):""
     } }; // bot name is case sensitive
 
@@ -63,8 +63,8 @@
     Refer below document for JWT token generation at server side. Client Id and Client secret should maintained at server end.
     https://developer.kore.ai/docs/bots/sdks/user-authorization-and-assertion/
     **/
-    botOptions.clientId = "cs-4496f8e8-a8b9-5d26-92f0-d4ed5660aca5";
-    botOptions.clientSecret = "iQM8ThGrLpB7AYw2QHH6c7pAUJnNtQIwSMkPrkZPiFQ=";
+    botOptions.clientId = "cs-cd4b4894-ef0f-5704-9731-022798c9b0ab";
+    botOptions.clientSecret = "+iiEoMRNfxOMvZVEoeaagNy59hLJaWU0g8lgcCJqrNI=";
     botOptions.brandingAPIUrl = botOptions.koreAPIUrl +'websdkthemes/'+  botOptions.botInfo._id+'/activetheme';
     botOptions.enableThemes = true;
 // for webhook based communication use following option 
